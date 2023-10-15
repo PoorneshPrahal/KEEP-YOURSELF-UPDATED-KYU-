c85b6f34d197dfc36da82ca52c7646b04125dca4
POORNESH PRAHALATHAN <83975683+PoorneshPrahal@users.noreply.github.com>
2023-10-15 12:41:12 +0530

    sidebar commit

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import "./App.css";
 import Home from "./Components/Home/home";
 
 import Navbar from "./Navbar.js";
-import Sidebar from "./Sidebar.js";
-import SecondNav from "./SecondNav.js";
+import Sidebar from "./Components/Sidebar";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Home />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Sidebar />
+        {/* <Home /> */}
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/Main.js
@@ -0,0 +1,8 @@
+import React from "react";
+import axios from "axios";
+const Main = () => {
+  useEffect(() => {});
+  return <div></div>;
+};
+
+export default Main;
