7447ed7cca8f4ff483caefa57a6dcbdb0dd1a9e5
POORNESH PRAHALATHAN <83975683+PoorneshPrahal@users.noreply.github.com>
2023-10-15 13:42:21 +0530

    sidebar commit

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,9 +1,27 @@
 import React from "react";
 import axios from "axios";
-import { useEffect } from "react";
+import Sidebar from "../Sidebar/Sidebar.js";
+import { useState, useEffect } from "react";
 const Main = () => {
-  useEffect(() => {});
-  return <div></div>;
+  const [tech, setTech] = useState([]);
+  useEffect(() => {
+    axios
+      .get(
+        "https://newsdata.io/api/1/news?apikey=pub_13638e5d227e5c4d3b68974d6e3b7841a6174&q=technology&size=3"
+      )
+      .then((res) => {
+        setTech(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  return (
+    <div>
+      <Sidebar />
+      <div style={{ display: "inline-block" }}></div>
+    </div>
+  );
 };
 
 export default Main;
