04aa2fd141064bc0474ff4be7d2396e21788a7ed
POORNESH PRAHALATHAN <83975683+PoorneshPrahal@users.noreply.github.com>
2023-10-16 09:24:34 +0530

    Main page

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,25 +1,92 @@
 import React from "react";
 import axios from "axios";
 import Sidebar from "../Sidebar/Sidebar.js";
-import { useState, useEffect } from "react";
+import Request from "../Requesting/Request.js";
+import { Link } from "react-router-dom";
+
 const Main = () => {
-  const [tech, setTech] = useState([]);
-  useEffect(() => {
-    axios
-      .get(
-        "https://newsdata.io/api/1/news?apikey=pub_13638e5d227e5c4d3b68974d6e3b7841a6174&q=technology&size=3"
-      )
-      .then((res) => {
-        setTech(res);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
   return (
-    <div>
+    <div style={{ display: "flex" }}>
       <Sidebar />
-      <div style={{ display: "inline-block" }}></div>
+      <div className="fullpage" style={{ width: "100%" }}>
+        <div
+          style={{
+            display: "inline-block",
+            width: "100%",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              marginLeft: "3%",
+              marginRight: "3%",
+            }}
+          >
+            <span style={{ fontSize: "1.7em", fontWeight: "600" }}>
+              Technology
+            </span>
+            <Link to="/" style={{ textDecoration: "none", color: "black" }}>
+              <span style={{ fontSize: "1.4em" }}>See all</span>
+            </Link>
+          </div>
+
+          <Request query="Technology" />
+        </div>
+
+        <div
+          style={{
+            display: "inline-block",
+            width: "100%",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              marginLeft: "3%",
+              marginRight: "3%",
+            }}
+          >
+            <span style={{ fontSize: "1.7em", fontWeight: "600" }}>
+              Technology
+            </span>
+            <Link to="/" style={{ textDecoration: "none", color: "black" }}>
+              <span style={{ fontSize: "1.4em" }}>See all</span>
+            </Link>
+          </div>
+
+          <Request query="Technology" />
+        </div>
+
+        <div
+          style={{
+            display: "inline-block",
+            width: "100%",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              marginLeft: "3%",
+              marginRight: "3%",
+            }}
+          >
+            <span style={{ fontSize: "1.7em", fontWeight: "600" }}>
+              Technology
+            </span>
+            <Link to="/" style={{ textDecoration: "none", color: "black" }}>
+              <span style={{ fontSize: "1.4em" }}>See all</span>
+            </Link>
+          </div>
+
+          <Request query="Technology" />
+        </div>
+      </div>
     </div>
   );
 };
